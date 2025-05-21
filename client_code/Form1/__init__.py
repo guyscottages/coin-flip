ab98e2d742b6611eda38b8d28b18be411af82fc0
guyscottages@gmail.com <guyscottages@gmail.com>
2025-05-21 18:55:28 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import random
+import time
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +9,13 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+
+
+def button_1_click(self, **event_args):
+  self.image_1.source = None
+  coins = ['http://re-bol.com/heads.jpg', 'http://re-bol.com/tails.jpg']
+  coin = random.choice(coins)
+  time.sleep(1)
+  self.image_1.source = URLMedia(coin)
+
